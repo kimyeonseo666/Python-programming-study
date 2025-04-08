44a3075318b1986669d36052d6a0b839c6b2bf78
김연서 <kim-yeonseo@gim-yeonseoui-MacBookAir.local>
2025-04-08 16:19:29 +0900

    2주차 스터디 완료

--- study2.py
+++ study2.py
@@ -10,4 +10,34 @@
 # print (A%B)
 
 for i in range (1,11) :
-    print (i)
+    print (i, end=' ')
+
+# num = int(input('숫자를 입력하시오'))
+# for i in range (1,num+1) :
+#     print (i)
+
+# num = int(input('숫자를 입력하시오.'))
+# for i in range (2, num+1, 2):
+#         print (i)
+
+# i = int(input('숫자를 입력하시오.'))
+
+# for i in range (1, i+1) :
+#     print ('*' *i ) 
+
+# number = int(input('정수 n을 입력하세요'))
+
+# for i in range (1, number+1)
+
+# number = int(input('숫자를 입력하세요'))
+
+
+# for i in range (1,101):
+#     print() 
+
+# n = int(input("n을 입력하세요"))
+# for i in range (1, n+1):
+#     print ('*'*i)
+
+
+
---
+++ test.py
@@ -0,0 +1,12 @@
+# a = 3
+# print(a)
+# print(%)
+
+# for i in range(1,10):
+#     print("2 * %d = %d" % (i, 2*i))
+
+for i in range(1, 6):
+    print("2 * %d = %d" % (i, 2 * i), end=' ')
+print()
+for i in range(6, 10):
+    print("2 * %d = %d" % (i, 2 * i), end=' ')
